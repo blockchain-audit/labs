7766e01a2f8b022e22d3be96f2923e25bcc2e7ac
sari341 <162886447+sari341@users.noreply.github.com>
2024-03-14 10:15:43 +0200

    Create Storage.sol

---
+++ src/Storage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract Storage {
+
+    uint256 number;
+
+     //setter
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    //getter
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
