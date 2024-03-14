7f5a9fc3f8d5b45a0b34e83dd4cfd313dc8db02d
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-14 15:06:30 +0200

    Update wallet.sol

--- src/wallet.sol
+++ src/wallet.sol
@@ -1,1 +1,25 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract wallet {
+    address payable public owner; 
+    mapping (address => uint256) public balances;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable { }
+
+    function receiveit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+    function withdraw(uint256 amount) public {
+        require(msg.sender == owner);
+        payable(owner).transfer(amount);
+    }
+    function balance() public view returns(uint){
+        return address(this).balance;
+    }
+}
