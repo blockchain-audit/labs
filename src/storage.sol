8a2124b20efab79ca2a58ec2ad866805e9f8900a
malka8580 <162886834+malka8580@users.noreply.github.com>
2024-03-14 10:24:57 +0200

    Create storage.sol

---
+++ src/storage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+
+contract Storage {
+
+    uint256 number;
+//sets the number
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    //gets the number
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
