d610433bf009ca6441f6a20a59db239bcc7e0334
sari341 <162886447+sari341@users.noreply.github.com>
2024-03-17 17:23:33 +0200

    Update Wallet.sol

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -2,24 +2,45 @@
 
 pragma solidity ^0.8.20;
 
+
 ///@title Wallet
 contract Wallet {
-
     address payable public owner;
+    address[] public owners;
 
     constructor() {
         owner = payable(msg.sender); // 'msg.sender' is sender of current call
+        owners.push(msg.sender);
     }
 
-receive() external payable {} 
+    receive() external payable {}
 
-    function withdraw(uint amount) external  {
-        require(msg.sender==owner,"WALLET-not-owner");
-        require(msg.sender.balance>=amount,"There is no enough money to withdraw");
+    function addOwner(address gabay) public isOwner {
+        require(owners.length < 3, "there are already 3 owners");
+        owners.push(gabay);
+    }
+
+    modifier isOwner() {
+        bool flag = false;
+        for (uint256 i = 0; i <= owners.length; i++) {
+            if (msg.sender == owners[i]) {
+                flag = true;
+                break;
+            }
+        }
+        require(flag, "Permission denied");
+        _;
+    }
+
+    function withdraw(uint256 amount) external isOwner {
+        require(
+            msg.sender.balance >= amount,
+            "There is no enough money to withdraw"
+        );
         payable(msg.sender).transfer(amount);
     }
 
-    function getBalance() external view returns(uint){
-        return  address(this).balance;
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
