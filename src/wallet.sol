35f71abd8279523bfae4168aea3a286ce43bdd68
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-17 12:16:57 +0200

    Update Wallet.sol
    more one owner

--- src/wallet.sol
+++ src/wallet.sol
@@ -1,25 +1,39 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <0.9.0;
 
 contract wallet {
-    address payable public owner; 
-    mapping (address => uint256) public balances;
+    address[] public owners; 
+    // mapping (address => uint256) public balances;
 
     constructor() {
-        owner = payable(msg.sender);
+        owners.push(msg.sender);
     }
 
-    receive() external payable { }
+     receive() external payable { }
 
-    function receiveit() external payable {
-        balances[msg.sender] += msg.value;
-    }
+    // function receiveit() external payable {
+    //     balances[msg.sender] += msg.value;
+    // }
     function withdraw(uint256 amount) public {
-        require(msg.sender == owner);
-        payable(owner).transfer(amount);
+        payable(msg.sender).transfer(amount);
     }
+   
+    function addOwner(address newOwner) public isOwner{
+            owners.push(newOwner);    
+        }
+        
     function balance() public view returns(uint){
         return address(this).balance;
+    }
+     modifier isOwner (){
+        bool isAdrsOwner = false;
+        for(uint i = 0; i < owners.length;i++){
+            if(msg.sender == owners[i]){
+               isAdrsOwner = true;
+               break;
+            }
+        }
+        require(isAdrsOwner ,"is not owner");
+    _;
     }
 }
