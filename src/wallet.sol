52f9fb97bab17d32c1c722d2b4840416bf15fbd2
shoshiGayer <121660686+shoshiGayer@users.noreply.github.com>
2024-03-17 16:35:18 +0200

    Update wallet.sol

--- src/wallet.sol
+++ src/wallet.sol
@@ -1,24 +1,28 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <0.9.0;
 
 contract wallet {
     address payable private owner;
-  
-    constructor()  {
+    address payable[] public gabaim;
+
+    constructor(address payable[] memory _gabaim) {
         owner = payable(msg.sender);
+        for (uint256 i = 0; i < _gabaim.length; i++) {
+            gabaim.push(_gabaim[i]);
+        }
     }
 
-     receive() external payable  {}
+    receive() external payable {}
 
-    function withdraw(uint wad) external {
-
+    function withdraw(uint256 wad) external {
+        require(
+            owner == msg.sender ||gabaim[0]==msg.sender||gabaim[1]==msg.sender||gabaim[2]==msg.sender,
+            "Only the owner and the gabaim allowed to withdraw Ether"
+        );
         payable(msg.sender).transfer(wad);
     }
 
-
-    function getBalance() public view returns (uint){
-
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-
 }
