ee312bf26607aee59a100b2235dfef4bf7478ba5
shoshiGayer <121660686+shoshiGayer@users.noreply.github.com>
2024-03-18 14:32:50 +0200

    Update wallet.sol

--- src/wallet.sol
+++ src/wallet.sol
@@ -3,20 +3,20 @@ pragma solidity >=0.6.12 <0.9.0;
 
 contract wallet {
     address payable private owner;
-    address payable[] public gabaim;
-
-    constructor(address payable[] memory _gabaim) {
+    //address payable[] public gabaim;
+    mapping (address=>uint256) public  hasgGabaim;
+    constructor() {
         owner = payable(msg.sender);
-        for (uint256 i = 0; i < _gabaim.length; i++) {
-            gabaim.push(_gabaim[i]);
-        }
+        hasgGabaim[0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d]=1;
+        hasgGabaim[0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f] =1;
+        hasgGabaim[0x0638cF39b33D063c557AE2bC4B5D22a790Ac8Fe4]=1;
     }
 
     receive() external payable {}
 
     function withdraw(uint256 wad) external {
         require(
-            owner == msg.sender ||gabaim[0]==msg.sender||gabaim[1]==msg.sender||gabaim[2]==msg.sender,
+            owner == msg.sender ||hasgGabaim[msg.sender]==1,
             "Only the owner and the gabaim allowed to withdraw Ether"
         );
         payable(msg.sender).transfer(wad);
