aad73a3d12d168b4b5c4daae9fe715ee82d5f332
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-18 12:50:52 +0000

    add walletGabaim and defi.d2

---
+++ src/WalletGabaiim.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract WalletGabaiim {
+    address public owner;
+    mapping (address => bool) gabaiim;
+    uint public countGabaiim = 0;
+
+
+
+    constructor() {
+        owner =  msg.sender;
+    }
+
+    receive() external payable { }
+
+    function withdraw(uint256 amount) public isOwnerOrGabai{
+        payable(msg.sender).transfer(amount);
+    }
+   
+    function addGabai(address newGabai) public {
+        require(msg.sender == owner ,"not owner");
+        require(gabaiim[newGabai],"the gabai already exsist");
+       if(countGabaiim < 3){
+            countGabaiim++;
+            gabaiim[msg.sender] = true;
+       }
+       else{
+        revert("there are already 3 gabaiim");
+       }
+    }
+        
+    function changeGabai(address oldGabai,address newGabai)public{
+        require(msg.sender==owner,"You do not have permission to do so");
+        require(!gabaiim[oldGabai],"the adress oldGabai not gabai");
+        require(gabaiim[newGabai], "the new gabai already gabai");
+        gabaiim[oldGabai] = false;
+        gabaiim[newGabai] = true;
+    }
+    function balance() public view returns(uint){
+        return address(this).balance;
+    }
+     modifier isOwnerOrGabai (){
+     require(msg.sender == owner ||gabaiim[msg.sender],
+     "sender is not owner or gabai" );
+    _;
+    }
+    
+}
