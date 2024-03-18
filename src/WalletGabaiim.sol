51b780b6aa7e3d2d7180b341742aa2ecafe20413
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-18 20:18:36 +0200

    Update WalletGabaiim.sol

--- src/WalletGabaiim.sol
+++ src/WalletGabaiim.sol
@@ -20,7 +20,7 @@ contract WalletGabaiim {
    
     function addGabai(address newGabai) public {
         require(msg.sender == owner ,"not owner");
-        require(gabaiim[newGabai],"the gabai already exsist");
+        require(!gabaiim[newGabai],"the gabai already exsist");
        if(countGabaiim < 3){
             countGabaiim++;
             gabaiim[msg.sender] = true;
@@ -32,18 +32,19 @@ contract WalletGabaiim {
         
     function changeGabai(address oldGabai,address newGabai)public{
         require(msg.sender==owner,"You do not have permission to do so");
-        require(!gabaiim[oldGabai],"the adress oldGabai not gabai");
-        require(gabaiim[newGabai], "the new gabai already gabai");
+        require(gabaiim[oldGabai],"the adress oldGabai not gabai");
+        require(!gabaiim[newGabai], "the new gabai already gabai");
         gabaiim[oldGabai] = false;
         gabaiim[newGabai] = true;
     }
+
     function balance() public view returns(uint){
         return address(this).balance;
     }
      modifier isOwnerOrGabai (){
-     require(msg.sender == owner ||gabaiim[msg.sender],
-     "sender is not owner or gabai" );
-    _;
+         require(msg.sender == owner || gabaiim[msg.sender],
+         "sender is not owner or gabai" );
+          _;
     }
     
 }
