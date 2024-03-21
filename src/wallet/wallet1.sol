611b86e1b05669cb0657c2db4a41aa76fdab829e
Your Name <name@name.io>
2024-03-21 07:30:24 +0000

    commit

---
+++ src/wallet/wallet1.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+contract Wallet1 {
+    mapping(address => bool) public owners;
+    address public mainOwner;
+    uint256 countOwners;
+
+    constructor() {
+        mainOwner = msg.sender;
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint wad) external isOwner {
+        payable (msg.sender).transfer(wad);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    modifier  isOwner() {
+
+        require(owners[msg.sender] == false || mainOwner == msg.sender,"Sender is not one of the owners");
+        _;
+    }
+
+    function addOwner(address newOwner) external {
+
+        require(msg.sender == mainOwner, "Only mainOwner can add another owner");
+        require(countOwners < 3, "There are already 3 owners");
+        require(owners[newOwner], "The owner is already exist");
+
+        countOwners++;
+        owners[newOwner] = true;
+
+
+    }
+
+    function changeOwner(address oldOwner, address newOwner) external {
+
+        require(msg.sender == mainOwner, "Only the main owner can change an owner");
+        require(!owners[oldOwner],"Adress of old owner does not exist as an owner");
+        require(owners[newOwner],"New owner is already an owner");
+        owners[oldOwner] = false;
+        owners[newOwner] = true;
+
+
+    }
+
+    function addOwner(address newOwner) external
+    {
+        require(msg.sender == mainOwner, "Only the main owner can add new owner");
+        owners.push(newOwner);
+    }
+
+
+ }
