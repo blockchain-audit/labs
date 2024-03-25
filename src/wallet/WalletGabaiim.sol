a589a40b86cadb712a08e6f4caf0d1fbe102d01b
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-25 19:14:00 +0200

    Update WalletGabaiim.sol

--- src/wallet/WalletGabaiim.sol
+++ src/wallet/WalletGabaiim.sol
@@ -2,58 +2,48 @@
 pragma solidity >=0.6.12 <0.9.0;
 
 import "forge-std/console.sol";
- 
+
 contract WalletGabaiim {
     address public owner;
-    mapping (address => bool)public gabaiim ;
-    uint public countGabaiim = 0;
-
-
+    mapping(address => bool) public gabaiim;
+    uint256 public countGabaiim = 0;
 
     constructor() {
-        owner =  msg.sender;
-        gabaiim[0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf] = true;
+        owner = msg.sender;
     }
 
-    receive() external payable { 
-        // console.log("in receive");
-    }
+    receive() external payable { }
 
-  
-    function withdraw(uint256 amount) public isOwnerOrGabai{
-        require(payable(address(this)).balance >= amount,"Not Enough Money in wallet");
-         console.log(address(this).balance);
+    function withdraw(uint256 amount) public isOwnerOrGabai {
+        require(payable(address(this)).balance >= amount, "Not Enough Money in wallet");
         payable(msg.sender).transfer(amount);
     }
-   
+
     function addGabai(address newGabai) public {
-        require(msg.sender == owner ,"not owner");
-        require(!gabaiim[newGabai],"the gabai already exsist");
-       if(countGabaiim < 3){
+        require(msg.sender == owner, "not owner");
+        require(!gabaiim[newGabai], "the gabai already exsist");
+        if (countGabaiim < 3) {
             countGabaiim++;
-            gabaiim[msg.sender] = true;
-       }
-       else{
-        revert("there are already 3 gabaiim");
-       }
+            gabaiim[newGabai] = true;
+        } else {
+            revert("there are already 3 gabaiim");
+        }
     }
-        
-    function changeGabai(address oldGabai,address newGabai)public{
-        require(msg.sender==owner,"You do not have permission to do so");
-        require(gabaiim[oldGabai],"the adress oldGabai not gabai");
+
+    function changeGabai(address oldGabai, address newGabai) public {
+        require(msg.sender == owner, "You do not have permission to do so");
+        require(gabaiim[oldGabai], "the adress oldGabai not gabai");
         require(!gabaiim[newGabai], "the new gabai already gabai");
         gabaiim[oldGabai] = false;
         gabaiim[newGabai] = true;
     }
 
-    function balance() public view returns(uint){
+    function balance() public view returns (uint256) {
         return address(this).balance;
     }
-     modifier isOwnerOrGabai (){
-        console.log(msg.sender);
-         require(msg.sender == owner || gabaiim[msg.sender],
-         "sender is not owner or gabai" );
-          _;
+
+    modifier isOwnerOrGabai() {
+        require(msg.sender == owner || gabaiim[msg.sender], "sender is not owner or gabai");
+        _;
     }
-    
 }
