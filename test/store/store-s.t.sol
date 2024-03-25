fcc14023030cc9ac061ebe24cba2fb8276608138
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-25 17:46:00 +0000

    tests

--- test/store/store-s.t.sol
+++ test/store/store-s.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "@hack/store/store.sol";
+import "@labs/store/store.sol";
 
 contract SimpleStoreTest is Test {
     /// @dev Address of the SimpleStore contract.
