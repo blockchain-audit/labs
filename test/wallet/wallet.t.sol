787b9d1c0cfed8d5e11c0bc8cc79058843dd0721
sari341 <162886447+sari341@users.noreply.github.com>
2024-03-27 14:11:16 +0200

    Update wallet.t.sol

--- test/wallet/wallet.t.sol
+++ test/wallet/wallet.t.sol
@@ -1,1 +1,10 @@
-gytyy
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@hack/wallet/wallet.sol";
+
+contract WalletTest is Test {
+    Wallet public w;
+    address public myUser = vm.addr(1);
