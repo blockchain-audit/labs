e73cefdea42532ce3797c2e9540e2eeddf7c97a4
sari341 <162886447+sari341@users.noreply.github.com>
2024-03-27 14:24:11 +0200

    Update Owner.sol

--- src/sheets/Owner.sol
+++ src/sheets/Owner.sol
@@ -1,3 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+
+contract Owner {
+
+    address private owner;
+
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    // modifier to check if caller is owner
+    modifier isOwner() {
+    //check if caller is owner.if not - print:"Caller is not owner"
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+     constructor() {
+        owner = msg.sender; // 'msg.sender' is sender of current call
+        emit OwnerSet(address(0), owner);
+    }
+
+   
+     //if isOwner==true, change the owner to be the new
+
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+  
+    // return address of owner
+     
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
