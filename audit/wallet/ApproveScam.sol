f24ae890df1191a581313c6805f68f59632fd5c5
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-28 14:59:03 +0000

    add audit

--- audit/wallet/ApproveScam.sol
+++ audit/wallet/ApproveScam.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.5.11;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
+
+//problem eve can to take from accounr alice tokens
 contract contractTest is Test {
     ERC20 ERC20Contract;
 
@@ -15,8 +17,46 @@ interface IERC20{
 
     function balanceOf(address account) external view returns (uint);
 
-    
+    function transfer(address recipient, uint amout) external returns (bool);
+
+    //?
+    function allowance(
+        address owner,
+        address spender 
+    ) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function tranferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint amount);
+
 }
 contract ERC20 is IERC20 {
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name =  "Test example";
+    string public symbol = "Tast";
+    uint public decimals = 18;
 
+    function transfer(address recipient, uint amount) external returns (bool){
+        balanceOf[msg.sender] -= amount;
+        balanceOf[receipent] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    // allowing to spender spend from sender amount tokens
+    function approve(address spender, uint amount) external returns (bool){
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender,amount);
+        return true;
+    }
+    
 }
