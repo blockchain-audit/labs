8bf1b0397bd029abbbbdff3536cfbde2e0b63d93
Chana-C <142223141+Chana-C@users.noreply.github.com>
2024-03-31 00:30:43 +0000

    Add change on vs to git

--- src/wallet/walletCollectors .sol
+++ src/wallet/walletCollectors .sol
@@ -8,19 +8,25 @@ install the audit plugin here or another solidty vscode extension in order to se
 when you finish, update the column T2-SOL in our sheet file, with 1 when you finished
 give a thumb here also
 */
+// Setting the Solidity version in which the contract can run
 
+// Setting the Solidity version in which the contract can run
 pragma solidity ^0.8.20;
 
+// The contract named Collectores
 contract Collectores {
+    // A public variable that represents the owner of the contract
     address public owner;
+    //Mapping that maps a collector's address to whether it belongs to one of three collectors or not
     mapping(address => bool) public collectors;
-
+    // Constructor function that builds the contract
     constructor() public {
         owner = msg.sender;
         // Owner is also a collector by default
+        // The contract owner is also a collector by default
         collectors[msg.sender] = true;
     }
-
+    
     modifier onlyOwner() {
         require(
             msg.sender == owner,
