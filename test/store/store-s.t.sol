a790a528faed815c5a92bdb933d1ad62f5edc99d
tova100 <tova0556768145@gmail.com>
2024-04-04 10:50:18 +0300

    update

--- test/store/store-s.t.sol
+++ test/store/store-s.t.sol
@@ -1,25 +1,25 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.15;
+// // SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.15;
 
-import "forge-std/Test.sol";
-import "forge-std/console.sol";
-import "@hack/store/store.sol";
+// import "forge-std/Test.sol";
+// import "forge-std/console.sol";
+// import "@hack/store/store.sol";
 
-contract SimpleStoreTest is Test {
-    // It works like a pointer to the
-    // Store contract
-    Store public s;
+// contract SimpleStoreTest is Test {
+//     // It works like a pointer to the
+//     // Store contract
+//     Store public s;
 
-    // Everything I need to start my test
-    function setUp() public {
-        // deploy the Store contract in the
-        // local blockchain node
-        s = new Store();
-    }
+//     // Everything I need to start my test
+//     function setUp() public {
+//         // deploy the Store contract in the
+//         // local blockchain node
+//         s = new Store();
+//     }
 
-    /// @dev Ensure that you can set and get the value.
-    function testSetAndGetValue(uint256 value) public {
-        s.setValue(value);
-        assertEq(value, s.getValue());
-    }
-}
+//     /// @dev Ensure that you can set and get the value.
+//     function testSetAndGetValue(uint256 value) public {
+//         s.setValue(value);
+//         assertEq(value, s.getValue());
+//     }
+// }
