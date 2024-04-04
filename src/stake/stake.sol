7c2a30f06b2a18a88609633837dcce5a787e139a
YehudisSternberg <yhs52274@gmail.com>
2024-04-04 17:05:45 +0300

    save

--- src/stake/stake.sol
+++ src/stake/stake.sol
@@ -15,19 +15,28 @@ mapping (address => uint ) public dates;
 
  function constructor(){
     owner = msg.sender;
-    totalReward = 1,000,000;
+    totalReward = 1000000;
     totalStaking = 0;
     
  }
 
 receive() external payable{}
 
+/// @dev a function that receives the staked coins and saves the date .
 function stakeing() external payable {
-
-
-    dates[msg.sender]= block.timestamp;
-    stakers[msg.sender]=msg.value;
+    require(msg.value > 0);
+    dates[msg.sender] = block.timestamp;
+    stakers[msg.sender] += msg.value;
     totalStaking += msg.value;
 }
+
+/// @dev a function that the staker couled pull his coins with geting his rewared
+function getrReward() external payable{
+    require(block.timestamp >= (dates[msg.value]+(7 days)),"you are not entitled to get a reward");
+    uint reward = (((100/(totalStaking/stakers[msg.sender]))*(totalReward))/100);
+    payable(msg.sender).send(reward);
+}
+
+
 
 }
