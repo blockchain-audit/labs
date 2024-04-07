004f9d9ae50eca3a4e6aa75480a59d0e17b7ee2d
shoshiGayer <920shoshi@gmail.com>
2024-04-07 12:53:27 +0300

    change

---
+++ src/Staking/Staking.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SimpleStaking {
+    mapping(uint256=>mapping(address=>uint256)public names) public dates;
+
+
+    constructor() {}
+
+   modifier onlyOwner(){
+
+   }
+
+    receive() external payable{}
+
+    function withdraw(uint256 wad) external{
+
+    } 
+    function ()
+
+}
