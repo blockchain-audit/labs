530fe87b9339384a1c728e0cd85b5d4916ec23d6
sari341 <s6799152@gmail.com>
2024-04-08 17:13:20 +0300

    add

--- src/staking/staking.sol
+++ src/staking/staking.sol
@@ -2,31 +2,26 @@
 // https://solidity-by-example.org/defi/staking-rewards/
 pragma solidity ^0.8.20;
 
-import "@hack/like/IERC20.sol";
+import "labs/new-project/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "....//new-project/src/MyToken.sol";
 
 contract StakingRewards {
     IERC20 public immutable stakingToken;
-    IERC20 public immutable rewardsToken;
+    MyToken public immutable rewardsToken;
     address public owner;
-    // uint public duration;
     uint public startAt;
     uint public updatedAt;
     uint public rewardRate;
-    // Sum of (reward rate * dt * 1e18 / total supply)
-    uint public rewardPerTokenStored;
-    // User address => rewardPerTokenStored per token paid
     mapping(address => uint) public deposits;
-    // User address => rewards to be claimed
-    // mapping(address => uint) public rewards;
-    // Total staked
     uint public totalSupply;
-    // User address => staked amount
-    mapping(address => uint) public startDate;
+    mapping(address =>uint) public startDate;
+    mapping(address =>uint) public rewards;
 
     constructor(address _stakingToken, address _rewardToken) {
         owner = msg.sender;
         stakingToken = IERC20(_stakingToken);
-        rewardsToken = IERC20(_rewardToken);
+        rewardsToken=new MyToken();
+
     }
 
     modifier onlyOwner() {
@@ -34,60 +29,34 @@ contract StakingRewards {
         _;
     }
 
-    modifier updateReward(address _account) {
-        rewardPerTokenStored = rewardPerToken();
-        updatedAt = lastTimeRewardApplicable();
 
-        if (_account != address(0)) {
-            rewards[_account] = earned(_account);
-            userReward[_account] = rewardPerTokenStored;
-        }
 
-        _;
-    }
-
-    function deposit(uint256 _amount) external () {
+    function deposit(uint256 _amount) external onlyOwner{
         require(_amount>0,"amount=0");
         stakingToken.transferFrom(msg.sender,address(this),_amount);
-        uint256 precentOfDeposit=_amount*100/(totalSupply+_amount);
+        totalSupply += _amount;
+        uint256 precentOfDeposit=_amount*100/totalSupply;
         deposits[msg.sender]+=precentOfDeposit;
-        totalSupply += _amount;
+        startDate[msg.sender]=block.timestamp;
+        getReward(_amount);
     }
 
-    function withdraw() external (){
-        
+    function getReward(uint256 _amount) external {
+        rewardsToken.transferFrom(address(this),msg.sender,_amount);
+        rewards[msg.sender]+=_amount;
+    }
+    modifier isSevenDays(){
+        uint today=block.timestamp;
+        require(today-startDate[msg.sender]>=7,"the reward duration is not finished yet");
     }
 
-
-
-
-
-    function lastTimeRewardApplicable() public view returns (uint) {
-        return _min(finishAt, block.timestamp);
+    function withdraw(uint _amount) external onlyOwner,isSevenDays{
+        require(_amount>0,"amount = 0");
+        require(deposits[msg.sender]>=_amount,"you don't have enouph tokens to withdraw");
+        uint calc = deposits[msg.sender]/rewards[msg.sender]*_amount;
+        deposits[msg.sender]-=calc;
+        stakingToken.transferFrom(address(this),msg.sender,calc);
+        totalSupply-=calc;
     }
 
-    function rewardPerToken() public view returns (uint) {
-        if (totalSupply == 0) {
-            return rewardPerTokenStored;
-        }
-
-        return
-            rewardPerTokenStored +
-            (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18) /
-            totalSupply;
-    }
-
-    function stake(uint _amount) external updateReward(msg.sender) {
-        require(_amount > 0, "amount = 0");
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
-        balanceOf[msg.sender] += _amount;
-        totalSupply += _amount;
-    }
-
-    function withdraw(uint _amount) external updateReward(msg.sender) {
-        require(_amount > 0, "amount = 0");
-        balanceOf[msg.sender] -= _amount;
-        totalSupply -= _amount;
-        stakingToken.transfer(msg.sender, _amount);
-    }
-
+}
