52e541c60138b964c6f7d569ca0dbab771aa76a0
YehudisSternberg <yhs52274@gmail.com>
2024-04-10 16:42:23 +0300

    save

--- test/staking/staking1.t.sol
+++ test/staking/staking1.t.sol
@@ -7,6 +7,15 @@ import "@hack/staking/staking1.sol";
 
 contract stakeTest is Test {
 
+    Staking1 public stake;
 
-
+   function setUp() public {
+        stake = new Staking1();  
+        // stake.mint(address(stake), stake.totalReward());
+    }
+    function test_staking() public {
+        console.log("total staking:", stake.totalStaking());
+        console.log("total staking:", stake.totalReward());
+        console.log("balance:", stake.balanceOf(address(stake)));
+        }
 }
