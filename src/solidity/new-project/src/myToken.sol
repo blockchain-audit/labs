f48aeaedcd96aee46a72f602aec6eb105b6be012
Chana-C <co025850257@gamil.com>
2024-04-10 15:32:14 +0200

    change

--- src/solidity/new-project/src/myToken.sol
+++ src/solidity/new-project/src/myToken.sol
@@ -1,10 +1,3 @@
-/*
-contract MyERC20 is ERC20 {
-    constructor() ERC20("Name", "SYM") {
-        this;
-    }
-}
-*/
 pragma solidity ^0.8.20;
 
 import "forge-std/console.sol";
@@ -23,19 +16,5 @@ contract MyToken is ERC20 {
 
     function mint(address myAddress, uint amount) public {
         _mint(myAddress, amount);
-    }
-}
-
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-
-//import "src/solidity/new-project/src/myToken.sol";
-
-contract myTokenTest is ERC20 {
-    //myToken public MyToken;
-    constructor(uint initialBalance) ERC20("MyToken", "MTK") {
-        _mint(msg.sender, initialBalance);
     }
 }
