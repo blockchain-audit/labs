4bda10ebd1b884b4f06985082083d9acb7014450
Chana-C <co025850257@gamil.com>
2024-04-30 19:28:40 +0300

    update

--- src/solidity/staking/myERC20.sol
+++ src/solidity/staking/myERC20.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "@openzeppelin/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+//import "@openzeppelin/ERC20/extensions/ERC20Permit.sol";
 
 contract MyToken is ERC20{
 
