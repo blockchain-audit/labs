ea6079cd3fbe2a2b4038f243eefd143a69f5fe4c
Chana-C <co025850257@gamil.com>
2024-05-01 16:04:28 +0300

    update

--- test/solidity/staking/myStaking.t.sol
+++ test/solidity/staking/myStaking.t.sol
@@ -1,10 +1,14 @@
 
-import "forge-std/Test.sol";
-import "forge-std/console.sol";
-//import "@labs/src/solidity/staking.sol";
-
 pragma solidity ^0.8.20;
 
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@hack/solidity/staking/myStaking.sol";
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Vm.sol";
 
+contract myStakingTest is Test{
+
+}
 
 
--- test/solidity/staking/staking.t.sol
+++ test/solidity/staking/staking.t.sol
@@ -1,7 +1,11 @@
 
 pragma solidity ^0.8.20;
 
-import "";
+import "@hack/solidity/staking/staking.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Vm.sol";
 
 contract StakingTest is Test{
 
