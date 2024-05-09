ad966356cd69a475e29fdfc35de96b00405a4a12
HadasaHazan <dassish212@gmail.com>
2024-05-09 16:34:23 +0300

    add cpAmmTest

--- test/AMM/cpAmmTest.t.sol
+++ test/AMM/cpAmmTest.t.sol
@@ -7,6 +7,9 @@ import "../../src/staking/MyToken.sol";
 import "../../src/staking/MyToken2.sol";
 import "forge-std/console.sol";
 
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+
 contract cpAmmTest is Test{
     
     MyToken x;
@@ -20,16 +23,29 @@ contract cpAmmTest is Test{
 
         x.mint(200); 
         y.mint(200);
-        // x.approve(address(cpAmm),200);
-        // y.approve(address(cpAmm),200);
+        
+        x.approve(address(cpAmm),200);
+        y.approve(address(cpAmm),200);
     }
 
     function testAddLiquidity() public{
-        cpAmm.addLiquidity(100, 100);
+        uint myShares = cpAmm.addLiquidity(100, 100);
+        console.log(myShares,"myShares");
+        assertEq(cpAmm.totalSupply(), myShares);
         assertEq(x.balanceOf(address(cpAmm)), 100);
+        assertEq(y.balanceOf(address(cpAmm)), 100);
+        assertEq(cpAmm.balances(address(this)), myShares);
+
+        vm.expectRevert();
+        myShares = cpAmm.addLiquidity(0, 100);
     }
 
     function testSwap() public{
+        
+    }
+
+      function testRemoveLiquidity() public{
 
     }
+
 }
