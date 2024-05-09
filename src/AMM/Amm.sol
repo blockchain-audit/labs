f726962b04af8b6f20548d333f97008f9f37431a
sariKahan <sari8530879@gmail.com>
2024-05-09 11:34:05 +0300

    --

--- src/AMM/Amm.sol
+++ src/AMM/Amm.sol
@@ -1,4 +1,4 @@
-// // SPDX-License-Identifier: GPL-3.0
+//     //SPDX-License-Identifier: GPL-3.0
 // pragma solidity ^0.8.15;
 
 // import "@labs/staking/MyToken.sol";
@@ -27,15 +27,17 @@
 //         uint256 countB = calcCount(balanceA, balanceB, amountA, 1);
 //         tokenB.transfer(msg.sender, countB);
 //         balanceB -= countB;
+//         totalLiquidity = balanceA+balanceB;
 //     }
 
 //     function tradeBToA(uint256 amountB) public {
 //         require(amountB != 0, "amount = 0");
-//         tokenB.transferFrom(msg.sender, address(this), amount);
+//         tokenB.transferFrom(msg.sender, address(this), amountB);
 //         balanceB += amount;
 //         uint256 countA = calcCount(balanceA, balanceB, amountB, 2);
 //         tokenA.transfer(msg.sender, countA);
 //         balanceA -= countA;
+//         totalLiquidity = balanceA+balanceB;
 //     }
 
 //     function addLiquidity(uint256 amountA, uint256 amountB) public {
@@ -45,14 +47,17 @@
 //         balanceA += amountA;
 //         balanceB += amountB;
 //         liquidity[msg.sender] = amountA * getValueOfAPer1Token();
+//         totalLiquidity += amountA + amountB;
 //     }
 
 //     function removeAllLiquidity() public {
-//         countA = getValueOfAPer1Token() * liquidity[msg.sender];
-//         countB = getValueOfBPer1Token() * liquidity[msg.sender];
+//         countA = liquidity[msg.sender] / getValueOfAPer1Token();
+//         countB = liquidity[msg.sender] / getValueOfBPer1Token();
 //         tokenA.transfer(msg.sender, countA);
 //         balanceA -= countA;
 //         tokenB.transfer(msg.sender, countB);
+//         balanceB -= countB;
+//         totalLiquidity = balanceA + balanceB;
 //     }
 
 //     function calcCount(uint256 _balanceA, uint256 _balanceB, uint256 amount, uint256 kindOfToken)
@@ -65,10 +70,6 @@
 //             return (_balanceB * 1e18 / _balanceA) * amount / 1e18;
 //         }
 //         return (_balanceA * 1e18 / _balanceB) * amount / 1e18;
-//     }
-
-//     function ratioBetweenAToB() public {
-//         return balanceA / balanceB;
 //     }
 
 //     function getValueOfAPer1Token() public returns (uint256) {
