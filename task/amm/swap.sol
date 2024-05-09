5782b1b3ceb93c1189e3b41ce3aef944dbf7e974
HadasaHazan <150019011+HadasaHazan@users.noreply.github.com>
2024-05-09 06:27:45 +0000

    update git - amm

---
+++ task/amm/swap.sol
@@ -0,0 +1,14 @@
+addIn = a0
+amountIn = 5
+
+tokenIn = a0
+tokenOut = a1
+
+reserveIn = 21
+reserveOut = 42
+
+amountInWithFee = (5 * 997) / 1000 = 4.985
+amountOut = (42 * 4.985) / 21 + 4.985) = 8.057
+
+reserve0 = 21 + 4.985 = 25.985
+reserve1 = 42 - 8.057 = 33.943
