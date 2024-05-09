675dd69812badeb1fb0615122c478825223475e1
Chana-C <co025850257@gamil.com>
2024-05-09 12:01:39 +0300

    update

--- test/solidity/walletToIsrael/walletEth.t.sol
+++ test/solidity/walletToIsrael/walletEth.t.sol
@@ -2,13 +2,15 @@
 
     pragma solidity ^0.8.20;
 
-    import "@hack/solidity/walletEth.sol";
+    import "@hack/solidity/walletToIsrael/walletEth.sol";
     import "foundry-huff/HuffDeployer.sol";
     import "forge-std/Test.sol";
     import "forge-std/console.sol";
     import "forge-std/Vm.sol";
 
-    contract WalletTest is Test{
-
-        
+    contract WalletEthTest is Test{
+    
+        // function setUp(){
+            
+        // }
     }
