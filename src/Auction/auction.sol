2dc5ae220ba1e1442cfd4da5ae01dc34eb897ee8
HadasaHazan <dassish212@gmail.com>
2024-05-12 11:54:59 +0300

    fix errors in auction

--- src/Auction/auction.sol
+++ src/Auction/auction.sol
@@ -20,13 +20,13 @@ contract Auction{
         uint price;
     }
 
-    struct BidderPerAuction{
+    struct BiddersPerAuction{
         Bidder prevBidder;
         Bidder currBidder;
     }
 
     mapping(address => Seller) public sellers;
-    mapping(address => BidderPerAuction) public auctions;
+    mapping(address => BiddersPerAuction) public auctions;
 
     event start(uint startDate, uint endAt);
     constructor() {
@@ -36,43 +36,43 @@ contract Auction{
     receive() external payable {}
 
     function initAuction(uint duration, IERC721 nft, uint tokenId, uint prePrice) public {
-        Seller seller;
+        Seller memory seller;
         seller.started = true;
         seller.startDate = block.timestamp;
         seller.endAt =  block.timestamp + duration;
         seller.NFTToken = nft;
-        Seller.tokenId = tokenId;
+        seller.tokenId = tokenId;
         seller.NFTToken.transferFrom(msg.sender, address(this), seller.tokenId);
         sellers[msg.sender] = seller; // new seller
         auctions[msg.sender].currBidder.bidAddr = msg.sender; // new auction
         auctions[msg.sender].currBidder.price = prePrice;
-        emit start(startDate, endAt);
+        emit start(seller.startDate, seller.endAt);
     }
 
     function addBid(uint price, address sellerAddr) external {
-        require(sellers[sellerAddr] != null, "incorrect address of seller"); // seller is exist
+        require(sellers[sellerAddr].tokenId != 0, "incorrect address of seller"); // seller is exist
         require(price > auctions[sellerAddr].currBidder.price, "price is low");
         auctions[sellerAddr].prevBidder = auctions[sellerAddr].currBidder;
         auctions[sellerAddr].currBidder.bidAddr = msg.sender;
         auctions[sellerAddr].currBidder.price = price; 
-        payable(address(this)).transferFrom(msg.sender, price);
+        // payable(address(this)).transfer(msg.sender, price);
     }
 
     function removeBid(address sellerAddr) external{
-        require(sellers[sellerAddr] != null, "incorrect address of seller"); // seller is exist
+        require(sellers[sellerAddr].tokenId != 0, "incorrect address of seller"); // seller is exist
         require(auctions[sellerAddr].prevBidder.bidAddr == msg.sender, "you can't cancel bid");
-       msg.sender.transfer(auctions[sellerAddr].prevBidder.price);        
-        auctions[sellerAddr].prevBidder = null;
+        // payable(msg.sender).transfer(auctions[sellerAddr].prevBidder.price);        
+        auctions[sellerAddr].prevBidder.bidAddr = address(0);
     }
 
     function endAuction(address sellerAddr) external{
-        require(sellers[sellerAddr] != null, "incorrect address of seller"); // seller is exist
+        require(sellers[sellerAddr].tokenId != 0, "incorrect address of seller"); // seller is exist
         require(sellers[sellerAddr].endAt <= block.timestamp, "auaction not finished!");        
         address highestBid = auctions[sellerAddr].currBidder.bidAddr;
         require(highestBid != sellerAddr, "the highest bid is the seller's bid");
-        if (auctions[sellerAddr].prevBidder != null){
-            payable(auctions[sellerAddr].prevBidder.bidAddr).transfer(auctions[sellerAddr].prevBidder.price);
-        }
+        // if (auctions[sellerAddr].prevBidder.bidAddr != address(0)){
+        //     payable(auctions[sellerAddr].prevBidder.bidAddr).transfer(auctions[sellerAddr].prevBidder.price);
+        // }
         sellers[sellerAddr].NFTToken.transferFrom(sellerAddr,highestBid, sellers[sellerAddr].tokenId);
     }
 }
---
+++ test/Auction/auctionTest.t.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+// https://solidity-by-example.org/defi/staking-rewards/
+// Code is a stripped down version of Synthetix
+pragma solidity ^0.8.24;
