86a7fbfc85889b8c6e600e681761cc90ab875272
MiriTenenboim <MiryamT@darkeychana.com>
2024-05-13 10:50:04 +0300

    Auction project

--- src/staking/stakingRewards.sol
+++ src/staking/stakingRewards.sol
@@ -3,27 +3,28 @@
 // Code is a stripped down version of Synthetix
 pragma solidity ^0.8.24;
 
-import "@hack/libs/ierc20.sol";
+import "forge-std/interfaces/IERC20.sol";
+import "forge-std/console.sol";
+
 
 contract StakingRewards {
     IERC20 public immutable stakingToken;
     IERC20 public immutable rewardsToken;
-
     address public owner;
-    uint256 public duration;        // [sec] time for payout
-    uint256 public finish = 0;      // [sec] finish reward time
-    uint256 public updated;         // [sec] last reward update
-    uint256 public rate = 7 days;   // [per] reward rate per sec
-    uint256 public acc;             // sum(reward rate * dt * 1e18 / total supply)
-    uint256 public totalSupply;                  // total staked
-    mapping(address => uint256) public paid;     // user reward per token paid
-    mapping(address => uint256) public rewards;  // reward to be claimed
-    mapping(address => uint256) public balanceOf;// staked per user
+    uint256 public duration = 7 days;   // [sec] reward duration
+    uint256 public finish   = 0;        // [sec] finish reward time
+    uint256 public updated;             // [sec] last time rate updated
+    uint256 public rate = 0;            // [per] reward rate per sec
+    uint256 public reward;              // reward per token stored
+    uint256 public staked;              // total staked
+    mapping(address => uint256) public paid;    // user reward per token paid
+    mapping(address => uint256) public rewards; // reward to be claimed
+    mapping(address => uint256) public balances;// staked per user
 
-    constructor(address _stakingToken, address _rewardToken) {
+    constructor(address st, address rt) {
         owner = msg.sender;
-        stakingToken = IERC20(_stakingToken);
-        rewardsToken = IERC20(_rewardToken);
+        stakingToken = IERC20(st);
+        rewardsToken = IERC20(rt);
     }
 
     modifier onlyOwner() {
@@ -31,80 +32,83 @@ contract StakingRewards {
         _;
     }
 
-    modifier updateReward(address _account) {
-        acc = accumulated();
-        updated = lastTime();
-        if (_account != address(0)) {
-            rewards[_account] = earned(_account);
-            paid[_account] = acc;
-        }
-        _;
-    }
-
-    /// --- VIEWS
-
+    // --- VIEWS
     function lastTime() public view returns (uint256) {
         return block.timestamp < finish ? block.timestamp : finish;
     }
 
     function accumulated() public view returns (uint256) {
-        if (totalSupply == 0) {
-            return acc;
+        if (staked == 0) {
+            return reward;
         }
-        return acc + (rate * (lastTime() - updated) * 1e18) / totalSupply;
+        return reward + (rate * (lastTime() - updated) * 1e18) / staked;
     }
 
-    function earned(address _account) public view returns (uint256) {
-        return ((balanceOf[_account] * (accumulated() - paid[_account])) / 1e18) + rewards[_account];
+    function earned(address guy) public view returns (uint256) {
+        return ((balances[guy] * (accumulated() - paid[guy])) / 1e18)
+                 + rewards[guy];
     }
 
     // --- STATE CHANGES
-
-    function stake(uint256 _amount) external updateReward(msg.sender) {
-        require(_amount > 0, "amount = 0");
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
-        balanceOf[msg.sender] += _amount;
-        totalSupply += _amount;
+    modifier updateReward(address guy) {
+        reward  = accumulated();
+        updated = lastTime();
+        if (guy != address(0)) {
+            rewards[guy] = earned(guy);
+            paid[guy]    = reward;
+        }
+        _;
     }
 
-    function withdraw(uint256 _amount) external updateReward(msg.sender) {
-        require(_amount > 0, "amount = 0");
-        balanceOf[msg.sender] -= _amount;
-        totalSupply -= _amount;
-        stakingToken.transfer(msg.sender, _amount);
+    function stake(uint256 amount) external updateReward(msg.sender) {
+        require(amount > 0, "amount = 0");
+        stakingToken.transferFrom(msg.sender, address(this), amount);
+        balances[msg.sender] += amount;
+        staked += amount;
+    }
+
+    function withdraw(uint256 amount) external updateReward(msg.sender) {
+        require(amount > 0, "amount = 0");
+        balances[msg.sender] -= amount;
+        staked -= amount;
+        stakingToken.transfer(msg.sender, amount);
     }
 
     function getReward() external updateReward(msg.sender) {
-        uint256 reward = rewards[msg.sender];
-        if (reward > 0) {
+        uint256 r = rewards[msg.sender];
+        if (r > 0) {
             rewards[msg.sender] = 0;
-            rewardsToken.transfer(msg.sender, reward);
+            rewardsToken.transfer(msg.sender, r);
         }
     }
 
     // --- ADMINISTRATION
-
     function setRewardsDuration(uint256 _duration) external onlyOwner {
         require(finish < block.timestamp, "reward duration not finished");
         duration = _duration;
     }
-
-    function notifyRewardAmount(uint256 amount) external onlyOwner updateReward(address(0)) {
+    
+    function updateRate(uint256 amount) external
+    onlyOwner updateReward(address(0)) {
+        console.log("block.timestamp", block.timestamp);
+        console.log("finish", finish);
         if (block.timestamp >= finish) {
             rate = amount / duration;
+            console.log("if");
         } else {
             uint remaining = (finish - block.timestamp);
-            uint leftover = remaining * rate;
+            uint leftover  = remaining * rate;
             rate = (amount + leftover) / duration;
+            console.log("else");
         }
-
-        // Ensure the provided reward amount is not more than the balance in the contract.
-        // This keeps the reward rate in the right range, preventing overflows due to
-        // very high values of rewardRate in the earned and rewardsPerToken functions;
-        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        // Ensure the provided reward amount is not more than the balance
+        // in the contract. This keeps the reward rate in the right
+        // range, preventing overflows due to very high values of
+        // rewardRate in the earned and rewardsPerToken functions;
+        // Reward + leftover must be less than 2^256 / 10^18 to
+        // avoid overflow.
         uint balance = rewardsToken.balanceOf(address(this));
         require(rate <= balance / duration, "provided reward too high");
-
         finish  = block.timestamp + duration;
         updated = block.timestamp;
     }
