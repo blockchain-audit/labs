105a8c21da9ee3c9e62cc9dbe126644602a876fa
Chana-C <co025850257@gamil.com>
2024-05-19 22:53:06 +0300

    update

--- src/buildLending/buildLending.sol
+++ src/buildLending/buildLending.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/ERC20/IERC20.sol";
+import "@openzeppelin/ERC20/ERC20.sol";
 import "@openzeppelin/ERC20/IERC20.sol";
 //import "";
 import "forge-std/console.sol";
