99012404670885a90e79f0bda828e6cea4f2c863
YehudisSternberg <yhs52274@gmail.com>
2024-05-19 13:20:23 +0300

    save

--- src/lending/lending.sol
+++ src/lending/lending.sol
@@ -11,33 +11,39 @@ contract lending is ERC20, Math1 {
 
     
 
-    uint256 public maxLTV;
+    uint256 public maxLTV = 4;
     uint256 public totalDeposit;
     uint256 public totalBorrowed;
     uint256 public totalCollateral;
     uint256 public totalReserve;
     uint256 constant ethPrice = 2900;
+    uint256 public baseRate = 20000000000000000;
+    uint256 public borrowRate = 300000000000000000;
     // mapping (address => uint256) usersDai;
     mapping (address => uint256) private usersCollateral;
     mapping (address => uint256) private usersBorrowed;
+
+    // address public owner;
     IERC20 public dai ;
 
     constructor(address tokenDai) ERC20("bond token" ,"bt"){
         dai = IERC20(tokenDai);
+        // owner = msg.sender;
+        
     }
 
     receive() external payable{}
 
     
-    modifier owner(){
+    modifier onlyOwner(){
        _; 
     }
-    modifier user(){
+    modifier onlyUser(){
         _;
     }
 
 
-    function bond(uint256 amount) external user{
+    function bond(uint256 amount) external onlyUser{
         require(amount > 0 );
         require(dai.balanceOf(msg.sender) >= amount, "you do not have enough ballance");
         dai.transferFrom(msg.sender , address(this), amount); 
@@ -48,22 +54,22 @@ contract lending is ERC20, Math1 {
        
     }
 
-    function unbond( uint256 amount) external user{
+    function unbond( uint256 amount) external onlyUser{
         require ( amount>0);
         require(balanceOf(msg.sender) >= amount, "you do not have enough tokens");
         dai.transferFrom(address(this), msg.sender, amount);
         uint256 daiToReceive = mulExp(amount ,getExchangeRate() );
         _burn(msg.sender, daiToReceive);
-         totalDeposit -= amount;
+        totalDeposit -= amount;
         
     }
-    function addCollateral() payable external user{
+    function addCollateral() payable external onlyUser{
         require( msg.value > 0 );
         usersCollateral[msg.sender] += msg.value;
         totalCollateral += msg.value;
         
     }
-    function removeCollateral( uint256 amount ) external user{
+    function removeCollateral( uint256 amount ) external onlyUser{
         require( amount > 0 );
         require ( usersCollateral[msg.sender] > 0,"you do not have any collaterals");
         uint256 barrowed = usersBorrowed[msg.sender];
@@ -76,22 +82,43 @@ contract lending is ERC20, Math1 {
         payable(msg.sender).transfer(amount);
 
     }
-    function borrowDai( uint256 amount) external user{
+    function borrowDai( uint256 amount) external onlyUser{
         require ( usersCollateral[msg.sender] > 0,"you do not have any collaterals");
         uint256 barrowed = usersBorrowed[msg.sender];
         uint256 collateral = usersCollateral[msg.sender];
+        uint256 left = mulExp(collateral, ethPrice) - barrowed;
+        uint256 borrowLimit = percentage(left, maxLTV);
+        require ( borrowLimit >= amount , "you cannot pass the limit");
+        dai.transferFrom(address(this) ,msg.sender, amount);
+        totalBorrowed += amount;baseRate;
 
     }
-    function repay() external user{
+    function repay( uint256 amount) external onlyUser{
+        require (usersBorrowed[msg.sender] > 0 , "you do not have any dept");
+        uint256 ratio = getExp(totalBorrowed, totalDeposit);
+        uint256 interestMul = getExp(borrowRate - baseRate , ratio);
+        uint256 rate = mulExp(ratio , interestMul) + baseRate;
+        uint256 fee = amount * rate;
+        uint256 paid = amount - fee;
+        totalReserve += fee;
+        usersBorrowed[msg.sender] -= paid;
+        totalBorrowed -= paid;
+    }
+    function trigger( address user ) external onlyOwner
+    {
+        uint256 barrowed = usersBorrowed[msg.sender];
+        uint256 collateral = usersCollateral[msg.sender];
+        uint256 left = mulExp(collateral, ethPrice) - barrowed;
+        // liquidationThreshold = percentage(left, maxLTV)
+
+
+
         
     }
-    function trigger() external owner{
+    function harvest() external onlyOwner{
         
     }
-    function harvest() external owner{
-        
-    }
-    function convert() external owner{
+    function convert() external onlyOwner{
         
     }
 
