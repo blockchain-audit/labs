0779bc3eef3d77af55ac7de3a1da0587b5dcc4c7
YehudisSternberg <yhs52274@gmail.com>
2024-05-28 15:43:45 +0300

    save

---
+++ src/interfaces/IWETHGateway.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.24;
+
+interface IWETHGateway {
+    function depositETH(
+        address lendingPool,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external payable;
+
+    function withdrawETH(
+        address lendingPool,
+        uint256 amount,
+        address onBehalfOf
+    ) external;
+}
