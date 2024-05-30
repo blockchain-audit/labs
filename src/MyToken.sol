470b6cb4a9749dd4cbac3a89d89c41e7e31c716f
sari341 <s6799152@gmail.com>
2024-05-30 11:06:53 +0300

    DdD

---
+++ src/MyToken.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        
+    }
+    function mint (address add,uint amount) external {
+        _mint(add,amount);
+    }
+
+    function burn(address account, uint256 value) external {
+          _burn(account,value);
+        }
+}
