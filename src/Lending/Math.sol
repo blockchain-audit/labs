1ac8594fcf711527cab0dbb30f0773b54ce3e8fb
sari341 <s6799152@gmail.com>
2024-06-03 16:30:00 +0300

    uhu

--- src/Lending/Math.sol
+++ src/Lending/Math.sol
@@ -1,34 +1,34 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.7;
+// //SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+// import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
-contract Math {
-    using SafeCast for uint256;
+// contract Math {
+//     using SafeCast for uint256;
 
-    uint256 private constant WAD = 1e18;
-    uint256 private constant HALF_WAD = WAD / 2;
+//     uint256 private constant WAD = 1e18;
+//     uint256 private constant HALF_WAD = WAD / 2;
 
-    function getExp(uint256 num, uint256 denom) internal pure returns (uint256) {
-        (bool successMul, uint256 scaledNumber) = num.tryMul(WAD);
-        if (!successMul) return 0;
-        (bool successDiv, uint256 rational) = scaledNumber.tryDiv(denom);
-        if (!successDiv) return 0;
-        return rational;
-    }
+//     function getExp(uint256 num, uint256 denom) internal pure returns (uint256) {
+//         (bool successMul, uint256 scaledNumber) = num.tryMul(WAD);
+//         if (!successMul) return 0;
+//         (bool successDiv, uint256 rational) = scaledNumber.tryDiv(denom);
+//         if (!successDiv) return 0;
+//         return rational;
+//     }
 
-    function mulExp(uint256 a, uint256 b) internal pure returns (uint256) {
-        (bool successMul, uint256 doubleScaledProduct) = a.tryMul(b);
-        if (!successMul) return 0;
-        (bool successAdd, uint256 doubleScaledProductWithHalfScale) = HALF_WAD.tryAdd(doubleScaledProduct);
-        if (!successAdd) return 0;
-        (bool successDiv, uint256 product) = doubleScaledProductWithHalfScale.tryDiv(WAD);
-        assert(successDiv == true);
-        return product;
-    }
+//     function mulExp(uint256 a, uint256 b) internal pure returns (uint256) {
+//         (bool successMul, uint256 doubleScaledProduct) = a.tryMul(b);
+//         if (!successMul) return 0;
+//         (bool successAdd, uint256 doubleScaledProductWithHalfScale) = HALF_WAD.tryAdd(doubleScaledProduct);
+//         if (!successAdd) return 0;
+//         (bool successDiv, uint256 product) = doubleScaledProductWithHalfScale.tryDiv(WAD);
+//         assert(successDiv == true);
+//         return product;
+//     }
 
-    function percentage(uint256 _num, uint256 _percentage) internal pure returns (uint256) {
-        uint256 rational = getExp(_num, 5);
-        return mulExp(rational, _percentage);
-    }
-}
+//     function percentage(uint256 _num, uint256 _percentage) internal pure returns (uint256) {
+//         uint256 rational = getExp(_num, 5);
+//         return mulExp(rational, _percentage);
+//     }
+// }
--- src/Lending/lending2.sol
+++ src/Lending/lending2.sol
@@ -1,75 +1,75 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../../src/MyToken.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "../../src/MyToken.sol";
 
-contract lending2 {
-    MyToken public immutable bDAI;
-    MyToken public immutable Dai;
-    mapping(address => uint256) public borrowers;
-    mapping(address => uint256) public collaterals;
-    mapping(address => uint256) public depositTime;
-    mapping(address => uint256) deposits;
-    address public owner;
-    uint256 public immutable WAD = 1e18;
-    uint256 public ratePerWeek;
+// contract lending2 {
+//     MyToken public immutable bDAI;
+//     MyToken public immutable Dai;
+//     mapping(address => uint256) public borrowers;
+//     mapping(address => uint256) public collaterals;
+//     mapping(address => uint256) public depositTime;
+//     mapping(address => uint256) deposits;
+//     address public owner;
+//     uint256 public immutable WAD = 1e18;
+//     uint256 public ratePerWeek;
 
-    constructor(address tok) {
-        address owner = msg.sender;
-        Dai = MyToken(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-        bDAI = MyToken(tok);
-    }
+//     constructor(address tok) {
+//         address owner = msg.sender;
+//         Dai = MyToken(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+//         bDAI = MyToken(tok);
+//     }
 
-    function deposit(uint256 amount) public {
-        require(
-            depositTime[msg.sender] == 0,
-            "You have already deposited Dai. withdraw your Dai before depositing more Dai."
-        );
-        Dai.transferFrom(msg.sender, address(this), amount);
-        bDAI.mint(msg.sender, amount);
-        depositTime[msg.sender] = block.timestamp;
-        deposits[msg.sender] = amount;
-    }
+//     function deposit(uint256 amount) public {
+//         require(
+//             depositTime[msg.sender] == 0,
+//             "You have already deposited Dai. withdraw your Dai before depositing more Dai."
+//         );
+//         Dai.transferFrom(msg.sender, address(this), amount);
+//         bDAI.mint(msg.sender, amount);
+//         depositTime[msg.sender] = block.timestamp;
+//         deposits[msg.sender] = amount;
+//     }
 
-    function calculateReward(address user, uint256 amount) private returns (uint256) {
-        uint256 rewardPerWeek = deposits[msg.sender] * ratePerWeek * amount;
-        uint256 week = 60 * 60 * 24 * 7;
-        uint256 duration = block.timestamp - depositTime[user];
-        return rewardPerWeek * duration / week;
-    }
+//     function calculateReward(address user, uint256 amount) private returns (uint256) {
+//         uint256 rewardPerWeek = deposits[msg.sender] * ratePerWeek * amount;
+//         uint256 week = 60 * 60 * 24 * 7;
+//         uint256 duration = block.timestamp - depositTime[user];
+//         return rewardPerWeek * duration / week;
+//     }
 
-    function ETHValue() public returns (uint256) {
-        uint256 _ETH = 3087;
-        return _ETH;
-    }
+//     function ETHValue() public returns (uint256) {
+//         uint256 _ETH = 3087;
+//         return _ETH;
+//     }
 
-    function unbond(uint256 amount) public {
-        require(amount > 0, "Invalid amount");
-        _token.burn(msg.sender, amount);
-        uint256 reward = calculateReward(msg.sender, amount);
-        Dai.transferFrom(address(this), msg.sender, amount + reward);
-    }
+//     function unbond(uint256 amount) public {
+//         require(amount > 0, "Invalid amount");
+//         _token.burn(msg.sender, amount);
+//         uint256 reward = calculateReward(msg.sender, amount);
+//         Dai.transferFrom(address(this), msg.sender, amount + reward);
+//     }
 
-    function addCollateral(uint256 amount) public payable {
-        payable(msg.sender).transfer(amount);
-        collaterals[msg.sender] += amount;
-    }
+//     function addCollateral(uint256 amount) public payable {
+//         payable(msg.sender).transfer(amount);
+//         collaterals[msg.sender] += amount;
+//     }
 
-    function removeCollateral(uint256 amount) public {
-        collaterals[msg.sender] -= amount;
-    }
+//     function removeCollateral(uint256 amount) public {
+//         collaterals[msg.sender] -= amount;
+//     }
 
-    function borrow(uint256 amount) public {
-        uint256 limit = ETHValue() * collaterals[msg.sender] - borrowers[msg.sender];
-        require(limit >= amount, "You don't have enough collaterals");
-        borrowers[msg.sender] += amount;
-        Dai.transferFrom(address(this), msg.sender, amount);
-    }
+//     function borrow(uint256 amount) public {
+//         uint256 limit = ETHValue() * collaterals[msg.sender] - borrowers[msg.sender];
+//         require(limit >= amount, "You don't have enough collaterals");
+//         borrowers[msg.sender] += amount;
+//         Dai.transferFrom(address(this), msg.sender, amount);
+//     }
 
-    function calculateCommission() private returns (uint256) {}
+//     function calculateCommission() private returns (uint256) {}
 
-    function repayBorrow(uint256 amount) external {
-        require(amount > 0, "You can't repay this amount");
-    }
-}
+//     function repayBorrow(uint256 amount) external {
+//         require(amount > 0, "You can't repay this amount");
+//     }
+// }
