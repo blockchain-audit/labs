f2beed8b0d9e4ed45018e14cdece5f519849db06
malka8580 <9497.mr@gmail.com>
2024-06-03 17:45:37 +0300

    add a function to proxy

--- src/proxy/myProxy.sol
+++ src/proxy/myProxy.sol
@@ -205,3 +205,33 @@ contract ProxyAdmin {
         Proxy(proxy).upgradeTo(implementation);
     }
 }
+
+
+
+library StorageSlot {
+    struct AddressSlot {
+        address value;
+    }
+
+    function getAddressSlot(bytes32 slot)
+        internal
+        pure
+        returns (AddressSlot storage r)
+    {
+        assembly {
+            r.slot := slot
+        }
+    }
+}
+
+contract TestSlot {
+    bytes32 public constant slot = keccak256("TEST_SLOT");
+
+    function getSlot() external view returns (address) {
+        return StorageSlot.getAddressSlot(slot).value;
+    }
+
+    function writeSlot(address _addr) external {
+        StorageSlot.getAddressSlot(slot).value = _addr;
+    }
+}
