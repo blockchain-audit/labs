f3162d96bce0dae23c80f6efe42f0cfa8fa1c8e5
sari341 <s6799152@gmail.com>
2024-06-09 17:45:31 +0300

    update

---
+++ src/ERC4626/ERC4626.sol
@@ -0,0 +1,16 @@
+abstract contract ERC4626 is ERC20, ERC4626 {
+    using Math for uint256;
+
+    IERC20 private immutable _asset;
+    uint8 private immutable _underlyingDecimals;
+
+    constructor(IERC20 asset_) {
+        (bool success, uint8 assetDecimal) = _tryDetAssetDecimal(asset);
+        _tryDetAssetDecimal = success ? assetDecimal : 18;
+        _asset = asset_;
+    }
+
+    function _convertTcShares(uint256 assets, Math.Rounding rounding) internal view virtual returns (uint256) {
+        return assets.MulDive(totalSupply() + 10 ** _decimalsoffset(), totalAssets() + 1, rounding);
+    }
+}
