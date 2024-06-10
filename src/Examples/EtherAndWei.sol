b269c4f7004ecef782514102ab6fe1f122307cc1
sari341 <s6799152@gmail.com>
2024-06-10 14:09:47 +0300

    update

--- src/Examples/EtherAndWei.sol
+++ src/Examples/EtherAndWei.sol
@@ -1,8 +1,11 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.24;
+    // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
-// contract EtherUnits {
-//     uint256 public oneWei = 1 wei;
-//     bool public isOneWei = (oneWei == 1 wei);
-//     uint256 public oneGwei = 1 gwei;
-// }
+contract EtherUnits {
+    uint256 public oneWei = 1 wei;
+    bool public isOneWei = (oneWei == 1);
+    uint256 public oneGwei = 1 gwei;
+    bool public isOneGwei = (oneGwei == 1e9);
+    uint256 public oneEther = 1 ether;
+    bool public isOneEther = (oneEther == 1e18);
+}
