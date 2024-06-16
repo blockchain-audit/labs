edcd7852eedf02374cfce63f55ae0402602faf78
Chana-C <co025850257@gamil.com>
2024-06-16 19:48:38 +0300

    update

--- src/basicSolidity/basic.sol
+++ src/basicSolidity/basic.sol
@@ -165,16 +165,16 @@ contract Mapping{
 contract NestedMapping{
     mapping (address => mapping(uint => bool)) public nested;
 
-    function get(address _addr, uint i) public view returns(uint){
+    function get(address _addr, uint i) public view returns(bool){
         return nested[_addr][i];
     }
 
     function set(address _addr, uint i, bool _boo) public{
-        myMap[_addr][i] = _boo;
+        nested[_addr][i] = _boo;
     }
 
     function remove(address _addr, uint i) public{
-        delete myMap[_addr][i];
+        delete nested[_addr][i];
     }
 }
 
@@ -186,5 +186,401 @@ contract Array{
     function get(uint i) public view returns(uint){
         return arr[i];
     }
+
+    function getArr() public view returns(uint256 [] memory){
+        return arr;
+    }
+
+    function push(uint i) public{
+        arr.push(i);
+    }
+
+    function pop() public{
+        arr.pop();
+    }
+
+    function getLength() public view returns(uint256){
+        return arr.length;
+    }
+
+    function remove(uint index) public {
+        delete arr[index];
+    }
+
+    function examples() external {
+        uint [] memory a = new uint[](5);
+    }
 }
+
+contract ArrayRemoveByShifting{
+    uint [] public arr;
+ 
+    function remove(uint _index) public {
+        require(_index < arr.length, "index out of bound.");
+        for(uint i = _index; i < arr.length - 1; i ++){
+            arr[i] = arr[i + 1];
+        }
+        arr.pop();
+    }
+
+    function test() external {
+        arr = [1, 2, 3, 4, 5];
+        remove(2); // [1, 2, 4, 5]
+        assert(arr[0] == 1);
+        assert(arr[1] == 2);
+        assert(arr[2] == 4);
+        assert(arr[3] == 5);
+        assert(arr.length == 4);
+        arr = [1];
+        remove(0); // []
+        assert(arr.length == 0);
+    }
+}
+
+contract ArrayReplaceFromEnd{
+    uint [] public arr;
+
+    function remove(uint index) public {
+
+        arr[index] = arr[arr.length -1];
+        // Remove the last element
+        arr.pop();
+    }
+
+    function test() public {
+        arr = [1, 2, 3, 4];
+
+        remove(1); // [1, 4, 3]
+
+        assert(arr.length == 3);
+        assert(arr[0] == 1);
+        assert(arr[1] == 4);
+        assert(arr[2] == 3);
+
+        remove(2); // [1, 4]
+
+        assert(arr.length == 2);
+        assert(arr[0] == 1);
+        assert(arr[1] == 4);
+        
+
+    }
+}
+
+contract Enum {
+    enum Status{
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
+
+    function get() public view returns(Status) {
+         return status;
+    }
+
+    function set(Status _status) public {
+        status = _status;
+    }
+
+    function cnacel() public {
+        status = Status.Canceled;
+    }
+
+    function reset() public {
+        delete status;
+    }
+}
+
+contract Todos{
+    struct Todo{
+        string text;
+        bool completed;
+    }
+
+    Todo[] public todos;
+
+    function create(string calldata _text) public {
+        // 3 ways to initialize a struct
+        todos.push(Todo(_text, false));
+
+        todos.push(Todo({text: _text, completed: false}));
+
+        Todo memory todo;
+
+        todo.text = _text;
+        todo.completed = false;
+        todos.push(todo);
+    }
+
+    function get(uint _index) public view returns(string memory, bool completed){
+        Todo storage todo = todos[_index];
+        return (todo.text, todo.completed);
+    }
+
+    function updateText(uint _index, string calldata _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    function toggleCompleted(uint _index) public {
+        Todo storage todo = todos[_index];
+        todo.completed = !todo.completed;
+    }
+}
+
+// Storage : אכסון בזיכרון חיצוני 
+// קבוע ומשמש לאחסון נתונים לאורך חיי החוזה החכם. נתונים המאוחסנים בזיכרון זה נשמרים גם לאחר סיום ביצוע הפונקציה
+
+// Memory : איכסון פנימי - זיכרון זמני 
+// משמש רק במהלך ביצוע הפונקציה. כל הנתונים המאוחסנים בזיכרון נמחקים כאשר הפונקציה מסתיימת.
+// משתנים המוגרים כך זמניים ונמצאים בשימוש רק בתוך הפונקציה או הקריאות הפנימיות שלה.
+
+//Calldata : זיכרון המשמש לקריאה בלבד ואינו ניתן לשינוי
+// יעיל עבור פרמטרים של פונקציות חיצוניות
+// יעיל יותר לשימוש בקריאות חוזרות בגלל שהוא לא יוצר עותק של הנתונים
+// משמש לאחסון נתונים שהתקבלו מהקריאה לפונקציה. מיועד לפרמטרים של פונקציות ציבוריות
+
+
+contract DataLocations {
+    
+    uint256 [] public arr;
+
+    mapping(uint256 => address) map;
+
+    struct MyStruct {
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public {
+       _f(arr, map, myStructs[1]);
+
+        MyStruct storage myStruct = myStructs[1];
+
+        MyStruct memory myMemStruct = MyStruct(0);
+    }
+
+    function _f(uint[] storage _arr, mapping(uint => address) storage _map, MyStruct storage _myStruct) internal {
+        arr[1] = 1;
+        _map[1] = address(1);
+        _myStruct.foo = 5;
+    }
+
+    function g(uint [] memory _arr) public returns (uint[] memory) {
+        _arr[0] = 1;
+        return _arr;
+    }
+
+    function h(uint [] calldata _arr) external {
+        uint num = _arr[0];
+    }
+}
+  
+    //Make sure EVM version and VM set to Cancun
+
+    // Storage - data is stored on the blockchain
+    // Memory - data is cleared out after a function call
+    // Transient storage - data is cleared out after a transaction
+    interface ITest {
+        function val() external view returns(uint);
+        function test() external;
+    }
+   
+    contract Callback {
+        uint public val;
+
+        fallback() external {
+            val = ITest(msg.sender).val();
+        }
+        
+        function test(address target) external {
+            ITest(target).test();
+        }
+    }
+
+    contract TestStorage {
+        uint public val;
+
+        function test() public {
+            val = 123;
+            bytes memory b = "";
+            msg.sender.call(b);
+        }
+    }
+
+    contract TestTransientStorage {
+        bytes32 constant SLOT = 0;
+
+        function test() public {
+            assembly {
+                tstore(SLOT, 321)
+            }
+            bytes memory b = "";
+            msg.sender.call(b);
+        }
+
+        function val() public view returns(uint256 v){
+            assembly {
+                v := tload(SLOT)
+            }
+        }
+    }
+
+    contract ReentrancyGuard {
+        bool private locked;
+
+        modifier lock() {
+            require(!locked);
+            locked = true;
+            _;
+            locked = false;
+        }
+
+        // 35313 gas
+        function test() public lock {
+            bytes memory b = "";
+           msg.sender.call(b);
+        }
+    }
+
+    // contract ReentrancyGuardTransient {
+    //     bytes constant SLOT = 0;
+
+    //     modifier lock() {
+    //         assembly {
+    //             // revert(0, 0)  טווח הנתונים שהפונקציה revert תיקח ממקום בזיכרון כדי להחזיר, ובמקרה זה מדובר באפס, כלומר אין נתונים להחזיר.
+    //             if tload(SLOT) { revert(0, 0)}
+    //             tstore(SLOT, 1)
+    //         }
+    //         _;
+    //         assembly {
+    //             tstore(SLOT, 0)
+    //         }
+    //     }
+
+    //     // 21887 gas
+    //     function test() external lock {
+    //         bytes memory b = "";
+    //         msg.sender.call(b);
+    //     }
+    // }
+
+    contract Function {
+        function returnMany() public pure returns (uint, bool, uint) {
+            return (1, true, 2);
+        }
+
+        function named() public pure returns (uint x, bool b, uint y) {
+            return (1, true, 2);
+        }
+    
+        // Return values can be assigned to their name.
+        // In this case the return statement can be omitted.
+        function assigned() public pure returns (uint x, bool b, uint y) {
+            x = 1;
+            b = true;
+            y = 2;
+        }
+
+        function destructuringAssignments() public pure returns (uint, bool, uint, uint, uint) {
+            (uint i, bool b, uint j) = returnMany();
+
+            (uint x, , uint y) = (4, 5, 6);
+
+            return (i, b, j, x, y);
+        }
+
+        function arrayInput(uint [] memory arr) public {}
+
+        uint [] public arr;
+
+        function arrayOutput() public view returns (uint [] memory) {
+            return arr;
+        }
+    }
+
+       contract XYZ {
+         function someFuncWithManyInputs (
+            uint x, uint y, uint z, address a, bool b, string memory c) 
+            public pure returns (uint){}
+
+            function callFunc() external pure returns (uint) {
+                return someFuncWithManyInputs(1, 2, 3, address(0), true, "c");
+            }
+
+            function callFuncWithKeyValue() external pure returns (uint) {
+                return someFuncWithManyInputs({
+                    a: address(0),
+                    b: true,
+                    c: "c",
+                    x: 1,
+                    y: 2,
+                    z: 3
+                });
+            }
+       }
+
+       contract ViewAndPure {
+        uint public x = 1;
+
+        function addTox(uint y) public view returns (uint) {
+            return x + y;
+        }
+
+        function add(uint i, uint j) public pure returns (uint) {
+            return i + j;
+        }
+    }
+
+    contract Error {
+        function testRequire(uint _i) public pure {
+            require(_i > 10, "Input must be greater than 10");
+        }
+
+        function testRevert(uint _i) public pure {
+            if(_i <= 10)
+                revert("Input must be greater than 10");
+        }
+
+        uint public num;
+
+        function testAssert() public view {
+            assert(num == 0);
+        }
+
+        error InsufficientBalance(uint balance, uint withdrawAmount);
+
+        function testCustomError(uint _withdrawAmount) public view {
+            uint bal = address(this).balance;
+
+            if(bal < _withdrawAmount) {
+                revert InsufficientBalance({
+                    balance: bal;
+                    withdrawAmount: _withdrawAmount;
+                })
+            }
+        }
+    }
+
+    contract Account {
+        uint public balance;
+        uint public constant MAX_UINT = 2 ** 256 - 1;
+
+        function deposit(uint _amount) public {
+            uint oldBalance = balance;
+            uint newBalance = balance + _amount;
+
+            require(newBalance >= oldBalance, "Overflow");
+            balance = newBalance;
+
+            assert(balance >= oldBalance);
+        }
+
+        function withdraw
+    }
 
--- src/walletToIsrael/| Function Name | Sighash | Function Sig.md
+++
@@ -1,3 +0,0 @@
-| Function Name | Sighash    | Function Signature | 
-| ------------- | ---------- | ------------------ | 
-| mint | 40c10f19 | mint(address,uint256) |
