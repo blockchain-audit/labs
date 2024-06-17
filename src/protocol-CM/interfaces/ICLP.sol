4bbca85dc4c06d8c219b456f14d984f95a966782
sari341 <s6799152@gmail.com>
2024-06-17 16:29:45 +0300

    delete a file

--- src/protocol-CM/interfaces/ICLP.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.13;
-
-interface ICLP {
-    function burn(address from, uint256 amount) external;
-
-    function mint(address to, uint256 amount) external;
-}
